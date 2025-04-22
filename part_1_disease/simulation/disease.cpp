f5e00eb37598fd9c23880abfd8499c52ea62b941
Prajesh Panjiyar <prajesh.panjiyar@stud.th-deg.de>
2025-04-22 22:42:31 +0200

    added definition method  for disease

--- part_1_disease/simulation/disease.cpp
+++ part_1_disease/simulation/disease.cpp
@@ -5,3 +5,11 @@ Disease::Disease(int dur, float trans_prob) {
     sick_duration = dur;
     transfer_probability = trans_prob;
 }
+
+int Disease::get_duration() { 
+    return sick_duration; 
+}
+
+float Disease::get_transfer_probability() { 
+    return transfer_probability; 
+}
