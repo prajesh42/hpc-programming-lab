0fe85bb75bfae1f64ecbb5bf5a29f1d18934c367
Prajesh Panjiyar <prajesh.panjiyar@stud.th-deg.de>
2025-05-16 01:07:57 +0200

    changes for file generation

--- part_1_disease/simulation/simulation.cpp
+++ part_1_disease/simulation/simulation.cpp
@@ -2,12 +2,13 @@
 
 #include <iostream>
 #include "INIReader.h"
+#include <fstream>
 
 Simulation::Simulation(std::string in_file) { input_file = in_file; }
 
 void Simulation::start() { std::cout << "Starting simulation..." << std::endl; 
 
-    INIReader reader("../configExample/disease_in.ini");
+    INIReader reader(input_file);
 
     if (reader.ParseError() < 0) {
         std::cerr << "Can't load 'disease_in.ini'\n";
@@ -34,6 +35,10 @@ void Simulation::start() { std::cout << "Starting simulation..." << std::endl;
 }
 
 void Simulation::run(int sim_runs, int population_size, std::string disease_name, int duration, float trans, std::string location_name, float vac_rate, bool patient_0) {
+    std::ofstream details("disease_details.csv");
+    std::ofstream stats("disease_stats.csv");
+    details << "name,infectious,recovered,susceptiple,vaccinated\n";
+    stats << "key,value\n";
     
     while(sim_runs-->0) {
         Population populace(population_size);
@@ -45,23 +50,44 @@ void Simulation::run(int sim_runs, int population_size, std::string disease_name
         int total_infected = 0;
         int step = 0;
 
+        std::cout << std::endl;
+        std::cout << "--------disease_details (" << disease_name << ") --------" << std::endl;
+        std::cout << "name,infectious,recovered,susceptiple,vaccinated" << std::endl;
+
         for (step = 0; ; ++step) {
             int infected_count = populace.count_infected();
+            int healthy = populace.count_healthy();
+            int recovered = population_size - infected_count - healthy - vaccinated;
+            total_infected += infected_count;
+            details << location_name << "," << infected_count << "," 
+                 << recovered << "," << healthy
+                 << "," << vaccinated << "\n";
+            std::cout << location_name << "," << infected_count << "," 
+                      << recovered << "," << healthy
+                      << "," << vaccinated << std::endl;
             if (infected_count == 0) {
                 break;
             }
             populace.one_more_day();
         }
+        details << "\n";
 
         int healthy = populace.count_healthy();
         int recovered = population_size - healthy - vaccinated;
-
         std::cout << std::endl;
         std::cout << "......... disease_stats (" << disease_name << ") ........." << std::endl;
-        std::cout << "        key         , value " << std::endl;
-        std::cout << "total_steps         , " << step << std::endl;
-        std::cout << "susceptible_persons , " << healthy << std::endl;
-        std::cout << "recovered_persons   , " << recovered << std::endl;
-        std::cout << "vaccinated_persons  , " << vaccinated << std::endl;
+        std::cout << "key,value " << std::endl;
+        stats << "total_steps," << step << "\n";
+        std::cout << "total_steps," << step << std::endl;
+        stats << "susceptible_persons," << healthy << "\n";
+        std::cout << "susceptible_persons," << healthy << std::endl;
+        stats << "recovered_persons," << recovered << "\n";
+        std::cout << "recovered_persons," << recovered << std::endl;
+        stats << "vaccinated_persons," << vaccinated << "\n\n";
+        std::cout << "vaccinated_persons," << vaccinated << std::endl;
+        std::cout << std::endl;
     }
+    
+    details.close();
+    stats.close();
 }
